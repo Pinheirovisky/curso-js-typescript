4ee608194dfcd4ffa585de2913f054d213a003d6
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-01-25 13:40:10 -0300

    feat: add 36th lesson

---
+++ src/A0034-interfaces/A0034-interfaces.ts
@@ -0,0 +1,36 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto: () => string;
+}
+
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto { }
+
+export class Pessoa implements TipoPessoa2 {
+  constructor(public nome: string, public sobrenome: string) { }
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoaObj: TipoPessoa2 = {
+  nomeCompleto() {
+    return this.nome + ' ';
+  },
+  nome: 'Vinicus',
+  sobrenome: 'Fechadíssimo',
+};
+
+const pessoa = new Pessoa('Vinicius', 'Pinheiro');
+
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
