fa3b5c1c46a20b6ead023578e3eeea2334873ebf
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-02-16 18:20:53 -0300

    feat: add 57th lesson

---
+++ src/A0057-decorator-class-5 copy/A0057-decorator-method.ts
@@ -0,0 +1,53 @@
+function decorador(
+  classPrototype: any,
+  nomeMetodo: string,
+  descriptor: PropertyDescriptor,
+): PropertyDescriptor {
+  console.log(classPrototype);
+  console.log(nomeMetodo);
+  console.log(descriptor);
+
+  return {
+    value: function (...args: string[]) {
+      return args[0].toUpperCase();
+    },
+    // writable: false,
+    // enumerable: false,
+    // configurable: false,
+  };
+}
+
+export class UmaPessoa {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  @decorador
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('Vinicius', 'Pinheiro', 30);
+// pessoa.metodo = () => 'EEEEEEEEEEI';
+const metodo = pessoa.metodo('Olá mundo!');
+
+console.log(metodo);
